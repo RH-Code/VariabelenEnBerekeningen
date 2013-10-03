9afc317e108c16bf970af17df561f11a54e20d70
Ruben H. <rhdesign.design@gmail.com>
2013-10-03 18:03:13 +0200

    Tijd van sec naar hh:mm:ss

---
+++ GeefDeTijdWeer/GeefDeTijdWeer/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GeefDeTijdWeer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTijdInSec = new System.Windows.Forms.TextBox();
+            this.btnZetOm = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtTijdInSec);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(121, 51);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Invoer tijd in sec.";
+            // 
+            // txtTijdInSec
+            // 
+            this.txtTijdInSec.Location = new System.Drawing.Point(6, 19);
+            this.txtTijdInSec.Name = "txtTijdInSec";
+            this.txtTijdInSec.Size = new System.Drawing.Size(100, 20);
+            this.txtTijdInSec.TabIndex = 0;
+            this.txtTijdInSec.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnZetOm
+            // 
+            this.btnZetOm.Location = new System.Drawing.Point(161, 12);
+            this.btnZetOm.Name = "btnZetOm";
+            this.btnZetOm.Size = new System.Drawing.Size(111, 51);
+            this.btnZetOm.TabIndex = 2;
+            this.btnZetOm.Text = "Tijd omzetten";
+            this.btnZetOm.UseVisualStyleBackColor = true;
+            this.btnZetOm.Click += new System.EventHandler(this.btnZetOm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 79);
+            this.Controls.Add(this.btnZetOm);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Geef tijd weer in hh:mm:ss";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtTijdInSec;
+        private System.Windows.Forms.Button btnZetOm;
+    }
+}
+
---
+++ GeefDeTijdWeer/GeefDeTijdWeer/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GeefDeTijdWeer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnZetOm_Click(object sender, EventArgs e)
+        {
+            int totaalAantalSeconden;
+            totaalAantalSeconden = Convert.ToInt32(txtTijdInSec.Text);
+
+            int uren = totaalAantalSeconden / 3600;
+            int seconden = totaalAantalSeconden % 3600;
+            int minuten = seconden / 60;
+            seconden = seconden % 60;
+
+            String resultaat = String.Format("{0} uur {1} minuten {2} seconden", uren, minuten, seconden);
+            MessageBox.Show(resultaat);
+        }
+    }
+}
