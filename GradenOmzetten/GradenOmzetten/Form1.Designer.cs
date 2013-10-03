b4857c8cd98766673caf52bb902957a282e001ec
Ruben H. <rhdesign.design@gmail.com>
2013-10-03 18:03:37 +0200

    °F --> °C en omgekeerd

---
+++ GradenOmzetten/GradenOmzetten/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace GradenOmzetten
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtGradenF = new System.Windows.Forms.TextBox();
+            this.btnZetOm = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtGradenC = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.txtGradenC2 = new System.Windows.Forms.TextBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.txtGradenF2 = new System.Windows.Forms.TextBox();
+            this.btnZetOm2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtGradenF);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(15, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(131, 53);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Graden in °F";
+            // 
+            // txtGradenF
+            // 
+            this.txtGradenF.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGradenF.Location = new System.Drawing.Point(6, 19);
+            this.txtGradenF.Name = "txtGradenF";
+            this.txtGradenF.Size = new System.Drawing.Size(100, 20);
+            this.txtGradenF.TabIndex = 0;
+            this.txtGradenF.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnZetOm
+            // 
+            this.btnZetOm.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnZetOm.Location = new System.Drawing.Point(15, 87);
+            this.btnZetOm.Name = "btnZetOm";
+            this.btnZetOm.Size = new System.Drawing.Size(131, 26);
+            this.btnZetOm.TabIndex = 1;
+            this.btnZetOm.Text = "Zet om";
+            this.btnZetOm.UseVisualStyleBackColor = true;
+            this.btnZetOm.Click += new System.EventHandler(this.btnZetOm_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtGradenC);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(15, 119);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(131, 53);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Graden in °C";
+            // 
+            // txtGradenC
+            // 
+            this.txtGradenC.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGradenC.Location = new System.Drawing.Point(6, 19);
+            this.txtGradenC.Name = "txtGradenC";
+            this.txtGradenC.Size = new System.Drawing.Size(100, 20);
+            this.txtGradenC.TabIndex = 2;
+            this.txtGradenC.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.groupBox1);
+            this.groupBox3.Controls.Add(this.groupBox2);
+            this.groupBox3.Controls.Add(this.btnZetOm);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.ForeColor = System.Drawing.Color.Red;
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(162, 189);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Van °F --> °C";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.groupBox5);
+            this.groupBox4.Controls.Add(this.groupBox6);
+            this.groupBox4.Controls.Add(this.btnZetOm2);
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.ForeColor = System.Drawing.Color.Red;
+            this.groupBox4.Location = new System.Drawing.Point(230, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(162, 189);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Van °C --> °F";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.txtGradenC2);
+            this.groupBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox5.Location = new System.Drawing.Point(15, 28);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(131, 53);
+            this.groupBox5.TabIndex = 0;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Graden in °C";
+            // 
+            // txtGradenC2
+            // 
+            this.txtGradenC2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGradenC2.Location = new System.Drawing.Point(6, 19);
+            this.txtGradenC2.Name = "txtGradenC2";
+            this.txtGradenC2.Size = new System.Drawing.Size(100, 20);
+            this.txtGradenC2.TabIndex = 0;
+            this.txtGradenC2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.txtGradenF2);
+            this.groupBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox6.Location = new System.Drawing.Point(15, 119);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(131, 53);
+            this.groupBox6.TabIndex = 1;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Graden in °F";
+            // 
+            // txtGradenF2
+            // 
+            this.txtGradenF2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGradenF2.Location = new System.Drawing.Point(6, 19);
+            this.txtGradenF2.Name = "txtGradenF2";
+            this.txtGradenF2.Size = new System.Drawing.Size(100, 20);
+            this.txtGradenF2.TabIndex = 2;
+            this.txtGradenF2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnZetOm2
+            // 
+            this.btnZetOm2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnZetOm2.Location = new System.Drawing.Point(15, 87);
+            this.btnZetOm2.Name = "btnZetOm2";
+            this.btnZetOm2.Size = new System.Drawing.Size(131, 26);
+            this.btnZetOm2.TabIndex = 1;
+            this.btnZetOm2.Text = "Zet om";
+            this.btnZetOm2.UseVisualStyleBackColor = true;
+            this.btnZetOm2.Click += new System.EventHandler(this.btnZetOm2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 217);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Form1";
+            this.Text = "Graden omzetten";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtGradenF;
+        private System.Windows.Forms.Button btnZetOm;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtGradenC;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox txtGradenC2;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.TextBox txtGradenF2;
+        private System.Windows.Forms.Button btnZetOm2;
+    }
+}
+
---
+++ GradenOmzetten/GradenOmzetten/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GradenOmzetten
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnZetOm_Click(object sender, EventArgs e)
+        {
+            double fahrenheit, celcius;
+            fahrenheit = Convert.ToDouble(txtGradenF.Text);
+            celcius = (fahrenheit - 32) * 5 / 9;
+
+            txtGradenC.Text = Convert.ToString(celcius);
+        }
+
+        private void btnZetOm2_Click(object sender, EventArgs e)
+        {
+            double fahrenheit2, celcius2;
+            celcius2 = Convert.ToDouble(txtGradenC2.Text);
+            fahrenheit2 = celcius2 * 9 / 5 + 32;
+
+            txtGradenF2.Text = Convert.ToString(fahrenheit2);
+        }
+
+        
+    }
+}
